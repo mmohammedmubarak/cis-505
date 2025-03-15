c4dbd3ddd780f86eb4a2c767427a5caefe9f87a7
Mohammad Abbas <abbas3324@gmail.com>
2025-03-15 12:43:23 -0500

    Adding solution to Module 1 HelloWorld Assignment

--- Module_1/HelloWorldApp/MohammedHelloWorld.java
+++ Module_1/HelloWorldApp/MohammedHelloWorld.java
@@ -1,22 +1,24 @@
 package Module_1.HelloWorldApp;
+
 public class MohammedHelloWorld {
-    /**
-     * @param args
-     */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         String firstName = "Abbas"; // Assigning value to firstName
         String lastName = "Mohammed"; // Assigning value to lastName
-        helloWorld(firstName, lastName); // Make a call to helloWorld() method with the values stored in the variables firstName and lastName
+        helloWorld(firstName, lastName); // Make a call to helloWorld() method with the values assigned to the variables
+                                         // firstName and lastName
     } // end main
 
     /**
-     * Method taking two arguments and prints the Hello World phrase with the first and last name
+     * Method taking two arguments and prints Hello World with the first and last
+     * name
+     * 
      * @param firstName String
-     * @param lastName String
-     * @return prints the first and last name prefixed with "Hello World from" text in the console
+     * @param lastName  String
+     * @return prints the first and last name prefixed with "Hello World from" text
+     *         in the console
      */
     public static void helloWorld(String firstName, String lastName) {
         System.out.printf("Hello World from " + firstName + " " + lastName);
-    }
+    } // end helloWorld
 
-}
+} // end MohammedHelloWorld
