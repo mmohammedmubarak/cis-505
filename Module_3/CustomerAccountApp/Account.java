4604d2016a6d830df53547ebc3b1b1d11888e6df
Mohammad Abbas <mmohammedmubarak@my365.bellevue.edu>
2025-03-29 00:03:47 -0500

    Module 3 Files

--- Module_3/CustomerAccountApp/Account.java
+++ Module_3/CustomerAccountApp/Account.java
@@ -7,8 +7,6 @@ package Module_3.CustomerAccountApp;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import Module_2.SportsTeamApp.TestSportsTeamApp;
-
 public class Account {
     private double balance = 200;
 
@@ -40,7 +38,7 @@ public class Account {
             balance = balance - amt;
         else {
             System.out.print("\n");
-            TestSportsTeamApp.printIndentedWithTwoSpaces("Withdrawal rejected due to insufficient balance\n");
+            TestCustomerAccountApp.printIndentedWithTwoSpaces("Withdrawal rejected due to insufficient balance\n");
         }
     } // end withdraw
 
@@ -48,11 +46,11 @@ public class Account {
      * Method to display menu to user to perform operation on the account
      */
     public void displayMenu() {
-        TestSportsTeamApp.printIndentedWithTwoSpaces("Account Menu\n");
-        TestSportsTeamApp.printIndentedWithTwoSpaces("Enter <D/d> for deposit\n");
-        TestSportsTeamApp.printIndentedWithTwoSpaces("Enter <W/w> for withdraw\n");
-        TestSportsTeamApp.printIndentedWithTwoSpaces("Enter <B/b> for balance\n");
-        TestSportsTeamApp.printIndentedWithTwoSpaces("  Enter option>: ");
+        TestCustomerAccountApp.printIndentedWithTwoSpaces("Account Menu\n");
+        TestCustomerAccountApp.printIndentedWithTwoSpaces("Enter <D/d> for deposit\n");
+        TestCustomerAccountApp.printIndentedWithTwoSpaces("Enter <W/w> for withdraw\n");
+        TestCustomerAccountApp.printIndentedWithTwoSpaces("Enter <B/b> for balance\n");
+        TestCustomerAccountApp.printIndentedWithTwoSpaces("  Enter option>: ");
     } // end displayMenu
 
     /**
--- Module_3/CustomerAccountApp/TestCustomerAccountApp.java
+++ Module_3/CustomerAccountApp/TestCustomerAccountApp.java
@@ -8,25 +8,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import Module_2.SportsTeamApp.TestSportsTeamApp;
-
 public class TestCustomerAccountApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        TestSportsTeamApp.printIndentedWithTwoSpaces("Welcome to the Customer Account App\n\n");
+        printIndentedWithTwoSpaces("Welcome to the Customer Account App\n\n");
         boolean validCustomerId;
         String customerId;
         do { // do while loop to prompt the user to enter customer id from given list
             validCustomerId = true;
-            TestSportsTeamApp.printIndentedWithTwoSpaces("Enter a customer ID:");
+            printIndentedWithTwoSpaces("Enter a customer ID:");
             System.out.print("\n");
-            TestSportsTeamApp.printIndentedWithTwoSpaces("  ex: 1007, 1008, 1009>: ");
+            printIndentedWithTwoSpaces("  ex: 1007, 1008, 1009>: ");
             List<String> customerIdList = Arrays.asList("1007", "1008", "1009");
             customerId = scanner.next();
             if (!customerIdList.contains(customerId)) {
                 System.out.print("\n");
-                TestSportsTeamApp.printIndentedWithTwoSpaces("Invalid customer id entered. Please try again\n\n");
+                printIndentedWithTwoSpaces("Invalid customer id entered. Please try again\n\n");
                 validCustomerId = false;
             }
         } while (validCustomerId == false);
@@ -42,43 +40,53 @@ public class TestCustomerAccountApp {
             String menuSelected = scanner.next();
             if (menuSelected.equalsIgnoreCase("d")) { // condition to handle deposit option
                 System.out.print("\n");
-                TestSportsTeamApp.printIndentedWithTwoSpaces("Enter deposit amount: ");
+                printIndentedWithTwoSpaces("Enter deposit amount: ");
                 account.deposit(scanner.nextDouble());
                 System.out.print("\n");
             } else if (menuSelected.equalsIgnoreCase("w")) { // condition to handle withdraw option
                 System.out.print("\n");
-                TestSportsTeamApp.printIndentedWithTwoSpaces("Enter withdraw amount: ");
+                printIndentedWithTwoSpaces("Enter withdraw amount: ");
                 account.withdraw(scanner.nextDouble());
                 System.out.print("\n");
             } else if (menuSelected.equalsIgnoreCase("b")) { // condition to display account balance
                 System.out.print("\n");
-                TestSportsTeamApp.printIndentedWithTwoSpaces("Account balance: ");
+                printIndentedWithTwoSpaces("Account balance: ");
                 System.out.printf("$%,6.2f", account.getBalance());
                 System.out.print("\n\n");
             } else { // condition to handle invalid option
                 System.out.print("\n");
-                TestSportsTeamApp.printIndentedWithTwoSpaces("Error: Invalid Option\n\n");
+                printIndentedWithTwoSpaces("Error: Invalid Option\n\n");
             }
             do { // do while loop to provide user option to continue with more operation
-                TestSportsTeamApp.printIndentedWithTwoSpaces("Continue? (y/n): ");
+                printIndentedWithTwoSpaces("Continue? (y/n): ");
                 continueOption = scanner.next();
                 if (continueOption.equalsIgnoreCase("y") || continueOption.equalsIgnoreCase("n"))
                     validContinueOption = true;
                 else
-                    TestSportsTeamApp.printIndentedWithTwoSpaces("Error: Invalid Option\n\n"); // condition to handle
-                                                                                               // invalid option
+                    printIndentedWithTwoSpaces("Error: Invalid Option\n\n"); // condition to handle
+                                                                             // invalid option
             } while (validContinueOption == false);
         } while (continueOption.equalsIgnoreCase("y"));
         System.out.print("\n\n");
         System.out.print(customer);
         System.out.print("\n\n");
-        TestSportsTeamApp.printIndentedWithTwoSpaces(
+        printIndentedWithTwoSpaces(
                 "Balance as of " + account.getTransactionDate() + " is "); // Display the current date in a specific
                                                                            // format
         System.out.printf("$%,6.2f", account.getBalance()); // Display the balance in a specific format
         System.out.print("\n\n");
-        TestSportsTeamApp.printIndentedWithTwoSpaces("End of line...");
+        printIndentedWithTwoSpaces("End of line...");
         scanner.close();
 
     } // end main
+
+    /**
+     * Method which prints the message by prefixing with two spaces as per
+     * assignment guidelines
+     * 
+     * @param printMessage
+     */
+    public static void printIndentedWithTwoSpaces(String printMessage) {
+        System.out.print("  " + printMessage);
+    } // end printIndentedWithTwoSpaces
 }
